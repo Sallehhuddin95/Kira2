8e499dd321cb4bfb5afe6a9b7e723d7b050ab282
Salleh Kamal <kamalsallehhuddin@yahoo.com>
2023-12-01 23:28:23 +0800

    feat: - add fooer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import "./App.css";
 import { MainTabs } from "./components";
+import { FaGithub } from "react-icons/fa6";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Welcome to Kira-Kira</h1>
       <MainTabs />
+      <footer className="text-center text-xs text-gray-500">
+        <p>Coded by Hacker</p>
+        <p>
+          <a
+            href="https://github.com/Sallehhuddin95/Kira2"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-1"
+          >
+            <span className="align-middle">GitHub</span>
+            <FaGithub className="ml-1 inline-block align-middle" />
+          </a>
+        </p>
+      </footer>
     </>
   );
 }
